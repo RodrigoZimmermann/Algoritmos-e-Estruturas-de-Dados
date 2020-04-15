3e6a4a8e91763596e5c77c8d4e5de40b642e187b
RodrigoZimmermann <rodrigo.l.zimmermann.rz@gmail.com>
2020-04-14 22:27:37 -0300

    Pilha

---
+++ Pilha/PilhaLista.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pilha;
+
+/**
+ *
+ * @author Rodrigo Luís Zimmermann
+ */
+public class PilhaLista<T> implements Pilha<T> {
+
+    private ElementoLista<T> topo;
+
+    @Override
+    public void push(T valor) throws Exception {
+        ElementoLista<T> novo = new ElementoLista<>();
+        if (this.vazia()) {
+            topo = new ElementoLista<>();
+            topo.setInfo(valor);
+        } else {
+            novo.setInfo(valor);
+            novo.setProx(topo);
+            topo = novo;
+        }
+    }
+
+//Métodos
+    @Override
+    public T pop() throws Exception {
+        T retorno;
+        if (this.vazia()) {
+            throw new ListaException("Não há nada na pilha");
+        } else {
+            retorno = (topo.getInfo());
+            topo = topo.getProx();
+        }
+        return retorno;
+    }
+
+    @Override
+    public T peek() {
+        T retorno;
+        if (this.vazia()) {
+            retorno = (T) "Pilha Vazia";
+        } else {
+            retorno = (topo.getInfo());
+        }
+        return retorno;
+    }
+
+    @Override
+    public boolean vazia() {
+        return topo == null;
+    }
+
+    @Override
+    public void libera() {
+        if (this.vazia()) {
+            throw new ListaException("Não há nada na pilha para liberar");
+        } else {
+            topo = topo.getProx();
+        }
+    }
+}
---
+++ Pilha/TesteLista.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pilha;
+
+/**
+ *
+ * @author Rodrigo Luís Zimmermann
+ */
+public class TesteLista {
+
+    public static void main(String[] args) throws Exception {
+        PilhaLista<String> pilha = new PilhaLista<>();
+        System.out.println("Esperado Pilha Vazia = " + pilha.peek());
+        pilha.push("A");
+        pilha.push("B");
+        System.out.println("Esperado B = " + pilha.peek());
+        pilha.push("C");
+        System.out.println("Esperado C = " + pilha.peek());
+        System.out.println("Esperado C = " + pilha.pop());
+        System.out.println("Esperado B = " + pilha.peek());
+        pilha.libera();
+        System.out.println("Esperado A = " + pilha.peek());
+        System.out.println("Esperado false " + pilha.vazia());
+        System.out.println("Esperado A = " + pilha.pop());
+        System.out.println("Esperado true " + pilha.vazia());
+    }
+}
